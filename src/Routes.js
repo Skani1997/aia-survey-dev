00cb94087f93f43078c01f39ad639fc836e0412d
seokani <i_m_tired@naver.com>
2020-09-23 12:06:49 +0900

    route change

--- src/Routes.js
+++ src/Routes.js
@@ -6,7 +6,7 @@ import Home from './Home';
 export default () => (
     <div >
       <BrowserRouter>
-        <Route exact={true} path="/aia-survey" component={Home}/>
+        <Route exact={true} path="/aia-survey-dev" component={Home}/>
         <Route path="/surveypage" component={SurveyP}/>
       </BrowserRouter>
     </div>
