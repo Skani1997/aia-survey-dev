87533a46b551f3cbd6728da9cffafb76f997cc1d
seokani <i_m_tired@naver.com>
2020-10-14 21:29:46 +0900

    favicon, second design

--- src/Components/HeaderNav.js
+++ src/Components/HeaderNav.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from '../image/logo1.png'
+import logo from '../image/dreamcalenderLogo.png'
 import "../css/HeaderNav.css"
 
 function HeaderNav() {
@@ -8,8 +8,9 @@ function HeaderNav() {
             <img className="logo" src={logo} alt=""></img>
             <div className="menu">
                 <ul>
-                    <li>상담요청</li>
-                    <li>설문하기</li>
+                    <li>드림캘린더 신청</li>
+                    <li>코드등록 & 검사하기</li>
+                    <li>고객센터</li>
                 </ul>
             </div>
         </nav>
