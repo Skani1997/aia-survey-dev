087bdfc49bcc27cfc115151bc61d0b4ab108f2ab
김석환 <kimseokhwan@kimseokhwanMacMini.local>
2020-12-31 18:18:10 +0900

    font

--- src/Components/HeaderContent.js
+++ src/Components/HeaderContent.js
@@ -3,7 +3,7 @@ import '../css/HeaderContent.css';
 
 function HeaderContent() {
     return (
-        <div>
+        <div id="headercontent-container">
             <h2 className="header-subtitle">당신의 꿈을 이뤄<span className="orange">드림</span></h2>
             <h1 className="header-title orange">드림캘린더</h1>
             <span className="description">
